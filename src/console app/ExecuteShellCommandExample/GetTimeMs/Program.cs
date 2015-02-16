73de43f6f9f11cf7fc53d28e1a5f87da528ee6a5
unknown <Tim.Smith@notamac.i3domain.inin.com>
2015-02-16 10:17:57 -0700

    adding files

---
+++ src/console app/ExecuteShellCommandExample/GetTimeMs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace GetTimeMs
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1)
+                {
+                    // Try to parse using the specified timestamp and a default format
+                    DateTime timestamp;
+                    if (DateTime.TryParse(args[0], out timestamp))
+                    {
+                        Environment.ExitCode = timestamp.Millisecond;
+                        return;
+                    }
+                }
+                else if (args.Length == 2)
+                {
+                    // Try to parse using the specified timestamp and format
+                    DateTime timestamp;
+                    if (DateTime.TryParseExact(args[0], args[1], CultureInfo.InvariantCulture, DateTimeStyles.None,
+                        out timestamp))
+                    {
+                        Environment.ExitCode = timestamp.Millisecond;
+                        return;
+                    }
+                }
+
+                // Neither condition was valid/worked, return the current timestamp's MS
+                Environment.ExitCode = DateTime.Now.Millisecond;
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Environment.ExitCode = -1;
+            }
+        }
+    }
+}
